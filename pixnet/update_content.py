794981bb0a3be4a0e844ac90acb1f322f536fa07
peihsuan <rabby410002@gmail.com>
2018-04-18 00:16:07 +0800

    Update update_content.py

--- pixnet/update_content.py
+++ pixnet/update_content.py
@@ -34,72 +34,7 @@ cursor2.execute("SET CHARACTER SET utf8mb4")
 cursor2.execute("SET character_set_connection=utf8mb4")
 cnx2.commit()
 
-'''
-def log_behavior(param,logtype):
-    global cursor
-    global cnx
 
-    sql = "insert into pixnet_log(param,logtype,logtime) values(%(param)s,%(logtype)s,NOW())"
-    data={'param':param,'logtype':logtype}
-    try:
-        cursor.execute(sql,data)
-        cnx.commit()
-    except: 
-        print('exception')
-        traceback.print_exc()
-
-
-
-def save_user(username,avatar,uniqueid,follow,name):
-    global cursor
-    global cnx
-
-    sql = "insert into pixnet_user(username,avatar,uniqueid,follow,name) values(%(username)s,%(avatar)s,%(uniqueid)s,%(follow)s,%(name)s)"
-    data={'username':username,'avatar':avatar,'uniqueid':uniqueid,'follow':follow,'name':name}
-    try:
-        cursor.execute(sql,data)
-        cnx.commit()
-    except: 
-        print('exception')
-        traceback.print_exc()
-
-
-def save_post(url,title,hit,pdate,category,tags,uuid):
-    global cursor
-    global cnx
-    tags=';'.join(tags)
-    sql = "insert into pixnet_post(url,title,hit,pdate,category,tags,uniqueuid) values(%(url)s,%(title)s,%(hit)s,%(pdate)s,%(category)s,%(tags)s,%(uuid)s)"
-    data={'url':url,'title':title,'pdate':pdate,'hit':hit,'category':category,'tags':tags,'uuid':uuid}
-
-    try:
-        cursor.execute(sql,data)
-        cnx.commit()
-    except: 
-        print('exception')
-        traceback.print_exc()
-
-
-def save_to_sql(jsondata):
-        data=jsondata['categoriesArticles']['data']
-        for dat in data:
-            save_user(dat['user']['user_name'],dat['user']['avatar'],dat['user']['member_uniqid'],dat['user']['follow_count'],dat['user']['name'])
-            save_post(dat['link'],dat['title'],dat['hit'],dat['published_date'],dat['category'],dat['tags'],dat['user']['member_uniqid'])
-#            save_user()
-    
-def update_page(js):
-    global cursor
-    global cnx
-    totalp=js['categoriesArticles']['totalPage']
-    curp=js['categoriesArticles']['currentPage']
-    if totalp!=curp:
-        sql = "update job_toplist set curpage="+str(curp+1)
-    try:
-        cursor.execute(sql)
-        cnx.commit()
-    except: 
-        print('exception')
-        traceback.print_exc()
-'''
 
 def myparser(content):
     global url
@@ -128,19 +63,7 @@ def myparser(content):
     crawlerutil.save_elements(cnx2,sql,data)
 
 
-'''
 
-def proc_post(pagenum):
-    crawlurl='https://styleme.pixnet.net/makeupsharing?page='+str(pagenum)
-    crawlerutil.crawl(crawlurl,myparser)
-
-
-def get_next_job():
-    sql = "select curpage from job_toplist"
-    cursor.execute(sql)
-    for (id) in cursor:
-        return id[0]
-'''
 sql = "select url from pix_list where content is null and ptime>=2015 order by rand() "
 #sql = "select url from pixnet_post_makeupsharing_temp where title = '-1' or subcategory is null or username is null"
 #sql = "select url from pixnet_post_makeupsharing where url ='http://styleme.pixnet.net/post/218390274'" #���疵嚙踐��
